7d7fca3a0ccc15f522ec35230fd60ac9caae8314
yoe174 <yoechannelpjr@gmail.com>
2025-06-09 12:07:43 +0700

    fecth url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 // src\services\auth.ts
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://be.masjidin.my.id';
 
 // export const login = async (email: string, password: string) => {
 //   const response = await fetch(`${API_URL}/api/login`, {
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,5 @@
 // utils/fetch.ts
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+// const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || "https://be.masjidin.my.id";
 
 export const apiUrl = (path: string) => `${API_BASE}${path}`;
